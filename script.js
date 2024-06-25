10e43ea8acf401a161a1b9a15642b1ae1218afca
SuziHarima <152184174+SuziHarima@users.noreply.github.com>
2024-06-25 19:57:17 -0300

    adiciona soma array

---
+++ script.js
@@ -0,0 +1,21 @@
+// Crie um array de numeros.
+//  Calcule e imprima a soma de todos os elementos do array. 
+// Utilize o reduce para realizar a soma.
+
+let numerosArray = [2, 5, 8, 14, 9];
+
+let somaArray = numerosArray.reduce((total, num) => total + num, 0)
+
+console.log(somaArray)
+
+
+// Crie um array chamado pares que contenha apenas os números pares do array numeros criado no exercício anterior. 
+// Em seguida, imprima esse novo array no console. 
+// Utilize o método filter para realizar a operação de filtragem.
+
+
+
+// Utilizando o array numeros utilizado nos exercícios anteriores, 
+// crie um novo array chamado quadrados que contenha o quadrado de cada número do array numeros. 
+// Imprima o array quadrados no console. 
+// Use o método map para realizar a criação de um novo array.
