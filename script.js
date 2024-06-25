30c1823cbd39f7591a4ece7d2985b393b6ff738d
SuziHarima <152184174+SuziHarima@users.noreply.github.com>
2024-06-25 20:04:59 -0300

    adiciona operação elevação ao quadrado array

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let numerosArray = [2, 5, 8, 14, 9];
 
-let somaArray = numerosArray.reduce((total, num) => total + num, 0)
+let somaArray = numerosArray.reduce((total, num) => total + num, 0);
 
-console.log(somaArray)
+console.log(somaArray);
 
 
 
 let paresArray = numerosArray.filter( n => {
     return n % 2 == 0
-})
+});
 
-console.log(paresArray)
+console.log(paresArray);
+
+
+let arrayQuadrado = numerosArray.map( n => {
+    return n ** 2
+});
+
+console.log(arrayQuadrado);
