0634adf72e465edd837d8511c6cd885197136299
SuziHarima <152184174+SuziHarima@users.noreply.github.com>
2024-06-25 21:00:29 -0300

    adiciona metoto mostrar estoque

--- script.js
+++ script.js
@@ -44,21 +44,28 @@ class Produto {
         this.quantidade += quantidadeReposta;
     }
 
+    mostrarEstoque(){
+        console.log("O produto " + this.nome + " possui " + this.quantidade + " unidades disponíveis.");
+    }
+
 }
 
 let produto1 = new Produto("manteiga", 3, 5);
+
 console.log("Quantidade inicial: " + produto1.quantidade);
 
 produto1.vender(3);
-
 console.log("Depois de vender: " + produto1.quantidade);
 
 produto1.repor(8);
-
 console.log("Depois de repor: " + produto1.quantidade);
 
+produto1.mostrarEstoque();
 
 
-// Crie um método chamado Repor() dentro da classe Produto.
 
-// Este método deve receber como parâmetro a quantidade reposta e adicionar o montante à quantidade do produto.
+// Crie um método chamado de MostrarEstoque() dentro da classe Produto.
+
+// Este método deve exibir uma mensagem parecida com a seguinte:
+
+// “O produto CANETA BIC AZUL possui 5 unidades disponíveis”
